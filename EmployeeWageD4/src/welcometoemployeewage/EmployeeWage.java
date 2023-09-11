e1a202b7b7f4247033b1c2ccbcc4d824a5c5f6d9
shilpa <shilpabhonde000@gmail.com>
2023-09-12 00:02:47 +0530

    UC2 Complete.

--- EmployeeWageD4/src/welcometoemployeewage/EmployeeWage.java
+++ EmployeeWageD4/src/welcometoemployeewage/EmployeeWage.java
@@ -1,14 +1,19 @@
 package welcometoemployeewage;
 
 public class EmployeeWage {
-    public static void main (String[]args)
+    public static void main(String[] args)
     {
-        int IS_FULL_TIME = 1;
-        double empcheck = Math.floor(Math.random() * 10 % 2);
+        int IS_FULL_TIME =1;
+        int EMPLOYEE_RATE_PER_HOUR =10;
 
-        if (empcheck == IS_FULL_TIME) {
+        double empcheck = Math.floor((Math.random() *10)% 2);
+
+        if (empcheck == IS_FULL_TIME)
+        {
             System.out.println("Employee is Present.");
-        } else {
+        }
+        else
+        {
             System.out.println("Employee is Absent.");
         }
     }
